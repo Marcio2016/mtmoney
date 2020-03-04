a0421c4e6d5bc291df1e6df620f3e75035700cae
Marcio2016 <marcio.tadeu2@gmail.com>
2020-03-03 21:23:31 -0300

    Criação do projeto

---
+++ src/main/java/com/mt/mtmoney/api/MtmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.mt.mtmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtmoneyApplication.class, args);
+	}
+
+}

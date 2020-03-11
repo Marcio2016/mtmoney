a725254215320171e993b902285de5714a812abd
Marcio2016 <marcio.tadeu2@gmail.com>
2020-03-10 23:13:09 -0300

    Consultando primeiro recurso

---
+++ src/main/java/com/mt/mtmoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.mt.mtmoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mt.mtmoney.api.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}

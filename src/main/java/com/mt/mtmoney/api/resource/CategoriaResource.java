38dcb099dac4381a8823b35c1489a8486f61dcbb
Marcio <marcio@localhost.localdomain>
2020-04-24 16:28:41 -0300

    usando eventos para header location

--- src/main/java/com/mt/mtmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/mt/mtmoney/api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.mt.mtmoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mt.mtmoney.api.evento.RecursoCriadoEvent;
 import com.mt.mtmoney.api.model.Categoria;
 import com.mt.mtmoney.api.repository.CategoriaRepository;
 
@@ -27,6 +28,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
@@ -34,13 +38,12 @@ public class CategoriaResource {
 	
 	@PostMapping	
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
-		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		Categoria categoriaSalva = categoriaRepository.save(categoria);				
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri();		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 		
 		//retorno 201 created e recurso salvo no header
-		return ResponseEntity.created(uri).body(categoriaSalva);				
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);				
 	}
 	
 	@GetMapping("/{codigo}")
--- src/main/java/com/mt/mtmoney/api/resource/PessoaResource.java
+++ src/main/java/com/mt/mtmoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.mt.mtmoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mt.mtmoney.api.evento.RecursoCriadoEvent;
 import com.mt.mtmoney.api.model.Pessoa;
 import com.mt.mtmoney.api.repository.PessoaRepository;
 
@@ -26,6 +27,9 @@ public class PessoaResource {
 
 	@Autowired
 	PessoaRepository repository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Pessoa> listar() {
@@ -45,10 +49,9 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
 		Pessoa pessoaSalva = repository.save(pessoa);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();	
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 		
 	}
 	

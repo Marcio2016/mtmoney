915cc813cb345a21f7d34618e275ee5eafb5ff1a
Marcio <marcio@localhost.localdomain>
2020-05-04 15:45:59 -0300

    Implementando update de lancamento

--- src/main/java/com/mt/mtmoney/api/service/LancamentoService.java
+++ src/main/java/com/mt/mtmoney/api/service/LancamentoService.java
@@ -3,12 +3,17 @@ package com.mt.mtmoney.api.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mt.mtmoney.api.model.Lancamento;
 import com.mt.mtmoney.api.model.Pessoa;
 import com.mt.mtmoney.api.repository.LancamentoRepository;
+import com.mt.mtmoney.api.repository.PessoaRepository;
 import com.mt.mtmoney.api.service.exception.PessoaInexistenteOuInativaException;
 
 @Service
@@ -16,6 +21,9 @@ public class LancamentoService {
 
 	@Autowired
 	private LancamentoRepository repository;
+	
+	@Autowired
+	PessoaRepository pessoaRepository;
 	
 	@Autowired
 	private PessoaService pessoaService;
@@ -42,5 +50,34 @@ public class LancamentoService {
 
 	public void remover(Long codigo) {
 		repository.deleteById(codigo);		
+	}
+
+	public Lancamento atualizar(Long codigo, @Valid Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return repository.save(lancamentoSalvo);
+	}
+	
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo())
+					.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		}
+
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+	
+	public Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = this.repository.findById(codigo)
+				.orElseThrow(() -> new IllegalArgumentException());
+				return lancamentoSalvo;		
 	}
 }

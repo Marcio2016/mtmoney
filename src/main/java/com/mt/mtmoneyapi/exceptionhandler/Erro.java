6bf9ff430f7253cb1de4c791bf82380242d18196
Marcio2016 <marcio.tadeu2@gmail.com>
2022-09-30 20:54:00 -0300

    adição do jackson.deserialization, messages.properties e implementação das classes de exception handler

---
+++ src/main/java/com/mt/mtmoneyapi/exceptionhandler/Erro.java
@@ -0,0 +1,12 @@
+package com.mt.mtmoneyapi.exceptionhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Erro {
+
+    private String mensagemUsuario;
+    private String mensagemDesenvolvedor;
+}

13e3e253e9069ff691094cd9def01f840335f572
Marcio2016 <marcio.tadeu2@gmail.com>
2022-09-30 12:30:58 -0300

    criação do projeto e configuração do datasource

---
+++ src/test/java/com/mt/mtmoneyapi/MtmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.mtmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtmoneyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
